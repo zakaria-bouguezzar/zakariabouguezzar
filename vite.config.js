74049e6feca06f49dac32af3958823f173b7fa7e
Zakariabouguezzar <zakariabouguezzar@gmail.com>
2025-09-20 13:07:10 +0100

    Updated animations and component

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 important for GitHub Pages
+  base: '/portfolio', // 👈 important for GitHub Pages
 })
